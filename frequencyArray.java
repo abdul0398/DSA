67ba1343451831e674e8a85e2ba3dcff36c4ba1d
Abdul_Rehman_Khan <abdulrkhan570@gmail.com>
2022-08-28 18:13:22 +0530

    Frequency array

---
+++ frequencyArray.java
@@ -0,0 +1,25 @@
+git import java.util.*;
+public class frequencyArray {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        int maxNo = Integer.MIN_VALUE;
+        int minNo = Integer.MAX_VALUE;
+        for(int i = 0;i < n;i++) {
+            arr[i] = sc.nextInt();
+            maxNo = Math.max(maxNo, arr[i]);
+            minNo = Math.min(minNo, arr[i]);
+        }
+
+
+        int[] freq = new int[maxNo + 1 - minNo];
+        for(int i = 0; i < n;i++){
+            freq[arr[i] - minNo ] = freq[arr[i] - minNo] + 1;
+        }
+        for(int i = 0; i < (maxNo - minNo +1); i++){
+            System.out.println((i + minNo) + " --> " + freq[i]);
+        }
+    }
+
+}
