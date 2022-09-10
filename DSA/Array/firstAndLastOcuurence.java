e912ddcce797761cd1c332e7551c964e49f668fe
Abdul_Rehman_Khan <abdulrkhan570@gmail.com>
2022-09-10 13:08:02 +0530

    added

---
+++ DSA/Array/firstAndLastOcuurence.java
@@ -0,0 +1,64 @@
+package DSA.Array;
+
+public class firstAndLastOcuurence {
+    public static void main(String[] args) {
+        int[] array = new int[]{1,2,2,3,4,6,7};
+        int firstIndex = firstOccurence(7, 2, array);
+        int lastIndex =lastOccurence(array, 7, 2);
+        System.out.println("first occurence is --> " + firstIndex + "\nlast occurence is --> " + lastIndex);
+    }
+
+    public static int firstOccurence(int size, int key, int arr[]) {
+
+            int start = 0;
+            int end = size - 1;
+            int mid = start + (end - start) / 2;
+            int ans = -1;
+            while (start <= end ) {
+                if (arr[mid] == key) {
+                    ans = mid;
+                    end = mid -1;
+
+                } else if (arr[mid] < key) {
+                    start = mid + 1;
+
+                } else if (arr[mid] > key) {
+                    end = mid - 1;
+                }
+                mid = start + (end - start) / 2;
+                
+            }
+            if (ans != 0){
+                return ans;
+            }else{
+                return -1;
+            }
+
+        }
+        public static int lastOccurence(int arr[], int size, int key ){
+            int start = 0;
+            int end = size -1;
+            int ans = -1;
+            int mid = start + (end - start) / 2;
+            while(start <= end){
+                if(arr[mid] == key){
+                    ans = mid;
+                    start = mid + 1;
+                }else if(arr[mid] > key){
+                    end = mid - 1;
+                }else{
+                    start = mid + 1;
+                }
+                mid = start + (end - start) / 2;
+            }
+            if(ans != 0){
+                return ans;
+            }else{
+                return -1;
+            }
+
+       
+    }
+}
+
+
--- DSA/Array/pactice.java
+++ DSA/Array/pactice.java
@@ -3,16 +3,4 @@ package DSA.Array;
 import java.util.*;
 
 public class pactice {
-    public static void main(String[] args) {
-        int[] array = new int[]{1,2,3,5,7,8,9,12,15};
-        int index = binary(array,9,7);
-        System.out.println(index);
-    }
-
-    public static int binary(int arr[], int size, int key ) {
-        
-    }
-            
-            
-        
     
