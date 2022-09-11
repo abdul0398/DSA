d30d97a24f1e4355a8564480893144466da20477
Abdul_Rehman_Khan <abdulrkhan570@gmail.com>
2022-09-11 23:38:11 +0530

    initial

--- DSA/Array/ArrayPartition.java
+++ DSA/Array/ArrayPartition.java
@@ -2,17 +2,10 @@ package DSA.Array;
 
 import java.util.Arrays;
 
-public class practice {
+public class ArrayPartition {
     public static void main(String[] args) {
         int[] arr = new int[]{1,4,3,2};
-        int digit = 0;
-        for(int i = 0; i < arr.length - 1; i++){
-            if(arr[i] > arr[i+1]){
-                 digit = arr[i];
-                 arr[i] = arr[i+1];
-                 arr[i+1] = digit;
-            }
-         }
+        Arrays.sort(arr);
          System.out.println(arr[1]);
         int sum = 0;
         for (int i = 0; i < arr.length; i = i+2) {
