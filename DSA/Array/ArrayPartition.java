33a3db2536546ebb2292168b6cef76421500dcd5
Abdul_Rehman_Khan <abdulrkhan570@gmail.com>
2022-09-12 18:50:43 +0530

    changes

--- DSA/Array/ArrayPartition.java
+++ DSA/Array/ArrayPartition.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 
 public class ArrayPartition {
     public static void main(String[] args) {
+// time complexity is O(nlogn)
         int[] arr = new int[]{1,4,3,2};
         Arrays.sort(arr);
          System.out.println(arr[1]);
@@ -12,5 +13,26 @@ public class ArrayPartition {
             sum = sum + arr[i];
         }
         System.out.println(sum);
+    }
+// time complexity is O(n)
+    public static int partition(int arr[]){
+        int max = Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
+        for (int i = 0; i < arr.length; i++) {
+            max = Math.max(arr[i], max);
+            min = Math.min(arr[i], min);
+        }
+        int[] freq = new int[max - min + 1];
+        for (int i = 0; i < arr.length; i++) {
+            freq[arr[i] - min ] = freq[arr[i] - min] + 1;
+        }
+        System.out.println(freq[5]);
+        int ans = 0;
+        int residue = 0;
+        for (int i = min; i <= max ; i++) {
+            ans = ans + (freq[i - min] + 1 - residue) / 2;
+            residue = (freq[i - min] + residue) % 2;
+        }
+        return ans;
     }
 }
--- DSA/Array/practice.java
+++ DSA/Array/practice.java
@@ -1,23 +1,29 @@
 package DSA.Array;
-
-import java.util.Arrays;
-
 public class practice {
     public static void main(String[] args) {
-        int[] arr = new int[]{1,4,3,2};
-        int digit = 0;
-        for(int i = 0; i < arr.length - 1; i++){
-            if(arr[i] > arr[i+1]){
-                 digit = arr[i];
-                 arr[i] = arr[i+1];
-                 arr[i+1] = digit;
-            }
-         }
-         System.out.println(arr[1]);
-        int sum = 0;
-        for (int i = 0; i < arr.length; i = i+2) {
-            sum = sum + arr[i];
+        int[] arr = new int[]{1,2,3,4,5,1,1,2,4,5};
+        int maxSum = partition(arr);
+        System.out.println(maxSum);
+    }
+// time complexity is O(n)
+    public static int partition(int arr[]){
+        int max = Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
+        for (int i = 0; i < arr.length; i++) {
+            max = Math.max(arr[i], max);
+            min = Math.min(arr[i], min);
         }
-        System.out.println(sum);
+        int[] freq = new int[max - min + 1];
+        for (int i = 0; i < arr.length; i++) {
+            freq[arr[i] - min ] = freq[arr[i] - min] + 1;
+        }
+        System.out.println(freq[5]);
+        int ans = 0;
+        int residue = 0;
+        for (int i = min; i <=max ; i++) {
+            ans = ans + (freq[i - min] + 1 - residue) / 2;
+            residue = (freq[i - min] + residue) % 2;
+        }
+        return ans;
     }
 }
