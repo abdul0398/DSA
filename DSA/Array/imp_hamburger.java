ff5263ed303256d4b11a56e977e806e907bf0e06
Abdul_Rehman_Khan <abdulrkhan570@gmail.com>
2022-09-15 22:46:48 +0530

    tweaks

---
+++ DSA/Array/imp_hamburger.java
@@ -0,0 +1,41 @@
+package DSA.Array;
+import java.util.*;
+public class imp_hamburger {
+    public static void main(String[] args) {
+        try (Scanner sc = new Scanner(System.in)) {
+            String sequence = sc.next();
+            int nb = sc.nextInt(), ns = sc.nextInt(), nc = sc.nextInt();
+            int pb = sc.nextInt(), ps = sc.nextInt(), pc = sc.nextInt();
+            long rubles = sc.nextLong();
+            long start = 0;
+            long end = 1000000010000L;
+            long mid = start + (end-start)/2;
+            long cb  = 0, cs = 0, cc = 0;
+            long ans = 0;
+            for (int i = 0; i < sequence.length(); i++) {
+                if(sequence.charAt(i) == 'B'){
+                    cb++;
+                }else if(sequence.charAt(i) == 'S'){
+                    cs++;
+                }else {
+                    cc++;
+                }
+            }
+            
+              while(end >= start){
+                long costOfBread =  ((mid * cb)-nb) > 0? ((mid * cb)-nb) * pb : 0;
+                long costOfSousage = ((mid * cs)-ns) > 0? ((mid * cs)-ns) * ps : 0;
+                long costOfcheese = ((mid * cc)-nc) > 0? ((mid * cc)-nc) * pc : 0;
+                long rublesNeeded =  costOfBread + costOfSousage + costOfcheese ;
+                if(rublesNeeded <= rubles){
+                    start = mid+1;
+                    ans = mid;
+                }else{
+                    end = mid - 1;
+                }
+                mid = start + (end-start)/2;
+            }
+            System.out.println(ans);
+    }
+}
+}
